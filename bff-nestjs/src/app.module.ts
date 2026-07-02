c02b6862e59c5496910d22988d10fd49db9e4c58
Jofred <reinosaj@gmail.com>
2026-07-02 17:09:10 -0400

    feat: sanitize root module and entry point using full naming conventions

--- bff-nestjs/src/app.module.ts
+++ bff-nestjs/src/app.module.ts
@@ -1,10 +1,8 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
 
 @Module({
+  controllers: [],
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
